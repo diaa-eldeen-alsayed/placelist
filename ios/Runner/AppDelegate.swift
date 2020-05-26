091099e33c41643be4e19021ee15f28fb5d16f04
diaa eldeen <diaaeldeen899@gmail.com>
2020-05-27 00:12:29 +0200

    first commit

---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,14 @@
+import UIKit
+import Flutter
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyAgkIbeaV5FvJU3bUc9SopWAuvW6pZUr9E")
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
